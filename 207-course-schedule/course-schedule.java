f5977c81c9d8d0e1c078fae1434998ff63baaae6
Bipin <bipinchoubey771@gmail.com>
2025-12-16 23:34:55 +0530

    Time: 7 ms (58.25%) | Memory: 47.1 MB (45.31%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+
+        for(int[] row : prerequisites){
+            int u = row[0];
+            int v = row[1];
+            adj.get(u).add(v);
+        }
+
+        int[] indegree = new int[numCourses];
+
+        for(int i = 0; i < numCourses; i++){
+            for(int neighbor : adj.get(i)){
+                indegree[neighbor]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.add(i);
+        }
+
+        int count = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            count++;
+
+            for(int neighbor : adj.get(node)){
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0){
+                    q.add(neighbor);
+                }
+            }
+        }
+        if(count == numCourses) return true;
+        return false;
+    }
+}
