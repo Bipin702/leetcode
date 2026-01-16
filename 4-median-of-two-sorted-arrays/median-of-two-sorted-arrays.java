80a725f3b65143c552b62ffc2c6c9ccf29bf0b54
Bipin <bipinchoubey771@gmail.com>
2026-01-16 15:31:54 +0530

    Time: 1 ms (99.56%) | Memory: 49.1 MB (21.87%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,40 +3,68 @@ class Solution {
         int n = nums1.length;
         int m = nums2.length;
 
-        int[] nums3 = new int[n+m];
+        int size = n+m;
+        int idx1 = 0;
+        int idx2 = 0;
+        if(size % 2 != 0){
+            idx1 = size/2;
+        }else{
+            idx1 = size/2 - 1;
+            idx2 = size/2;
+        }
+        int element1 = 0;
+        int element2 = 0;
         int i = 0, j = 0, k = 0;
 
         while(i < n && j < m){
             if(nums1[i] < nums2[j]){
-                nums3[k] = nums1[i];
+                if(k == idx1){
+                    element1 = nums1[i];
+                }
+                if(k == idx2){
+                    element2 = nums1[i];
+                }
                 i++;
             }else{
-                nums3[k] = nums2[j];
+                if(k == idx1){
+                    element1 = nums2[j];
+                }
+                if(k == idx2){
+                    element2 = nums2[j];
+                }
                 j++;
             }
             k++;
         }
 
         while(i < n){
-            nums3[k] = nums1[i];
+            if(k == idx1){
+                element1 = nums1[i];
+            }
+            if(k == idx2){
+                element2 = nums1[i];
+            }
             i++;
             k++;
         }
 
         while(j < m){
-            nums3[k] = nums2[j];
+            if(k == idx1){
+                element1 = nums2[j];
+            }
+            if(k == idx2){
+                element2 = nums2[j];
+            }
             j++;
             k++;
         }
 
         double median = 0;
 
-        int len = nums3.length;
-
-        if (len % 2 != 0) {
-            return nums3[len / 2];
+        if (size % 2 != 0) {
+            return element1;
         } else {
-            return (nums3[len / 2 - 1] + nums3[len / 2]) / 2.0;
+            return (element1 + element2) / 2.0;
         }
     }
 }
