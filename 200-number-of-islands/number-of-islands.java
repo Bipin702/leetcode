eb4da3982be1f0131a1b18c37cffaa7dc29cfccf
Bipin <bipinchoubey771@gmail.com>
2026-01-20 16:31:39 +0530

    Time: 4 ms (49.59%) | Memory: 54.4 MB (11.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -5,30 +5,33 @@ class Solution {
 
         int[][] visited = new int[n][m];
         int count = 0;
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == '1' && visited[i][j] == 0){
                     count++;
-                    dfs(i,j,visited,grid);
+                    dfs(grid,i,j,visited);
                 }
             }
         }
         return count;
     }
 
-    public void dfs(int row, int col, int[][] visited, char[][] grid){
+    public void dfs(char[][] grid, int row, int col, int[][] visited){
         visited[row][col] = 1;
+
         int n = grid.length;
         int m = grid[0].length;
+
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
 
-        for(int d = 0; d < 4; d++){
-            int nRow = row + delRow[d];
-            int nCol = col + delCol[d];
+        for(int i = 0; i < 4; i++){
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
 
-            if(nRow >= 0 && nRow < n && nCol >=0 && nCol < m && visited[nRow][nCol] == 0 && grid[nRow][nCol] == '1'){
-                dfs(nRow,nCol,visited,grid);
+            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' && visited[nRow][nCol] == 0){
+                dfs(grid,nRow, nCol, visited);
             }
         }
     }
