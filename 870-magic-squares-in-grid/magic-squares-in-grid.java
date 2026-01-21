3e44e1968d9847406a8652df7aec16f4c9c66b40
Bipin <bipinchoubey771@gmail.com>
2026-01-21 23:27:05 +0530

    Time: 1 ms (50.08%) | Memory: 43.5 MB (25.03%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.java
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -1,35 +1,51 @@
 class Solution {
-    public boolean isMagicSquare(int[][] grid,int r,int c){
-        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
-        HashSet<Integer> set = new HashSet<>();
- 
-        for(int i=0;i<3;i++){ // 1 - 9 checking
-            for(int j=0;j<3;j++){
-                int num = grid[r+i][c+j];
-                if(num<1 || num>9 || set.contains(num)) return false;
-                set.add(num);
+    public int numMagicSquaresInside(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int count = 0;
+
+        for(int i = 0; i <= n-3; i++){
+            for(int j = 0; j <= m-3; j++){
+                if(isMagicSquare(grid,i,j)) count++;
             }
         }
-        
-        for(int i=0;i<3;i++){ // row and col sum checking
-            if((grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i]) != sum) return false;
-            if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != sum) return false;
-        }
-
-        //diagonal sum checking
-        if((grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2]) != sum) return false;
-        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != sum) return false;
-        return true;
+        return count;
     }
 
-    public int numMagicSquaresInside(int[][] grid) {
-        int m = grid.length,n = grid[0].length;
-        int c = 0;
-        for(int i=0;i<=m-3;i++){
-            for(int j=0;j<=n-3;j++){
-                if(isMagicSquare(grid,i,j)) c++;
+    public boolean isMagicSquare(int[][] grid, int r, int c){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                int num = grid[r+i][c+j];
+
+                if(num < 1 || num > 9 || set.contains(num)){
+                    return false;
+                }else{
+                    set.add(num);
+                }
             }
         }
-        return c;
+
+        //Find sum are equal or not;
+        int rSum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+
+        for(int i = 0; i < 3; i++){
+            if(grid[r+i][c]+grid[r+i][c+1]+grid[r+i][c+2] != rSum){
+                return false;
+            }
+            
+            //col sum
+            if(grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i] != rSum){
+                return false;
+            }
+        }
+
+        // Diagonal and antidiagonal
+        if(grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != rSum) return false;
+
+        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != rSum) return false;
+
+        return true;
     }
 }
