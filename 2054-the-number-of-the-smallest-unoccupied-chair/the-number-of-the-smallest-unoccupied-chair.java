b3b4c978fd1e09828a8c51182edf6014aff3e453
Bipin <bipinchoubey771@gmail.com>
2026-01-24 16:38:34 +0530

    Time: 34 ms (86.36%) | Memory: 51.4 MB (76.70%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,24 +1,33 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        int n = times.length;
-
-        int[] chairs = new int[n];
-        int targetArrival = times[targetFriend][0];
-        Arrays.sort(times,(a,b) -> Integer.compare(a[0],b[0]));
-        for(int[] time : times){
-            int arrival = time[0];
-            int depart = time[1];
-
-            for(int i = 0; i < n; i++){
-                if(chairs[i] <= arrival){
-                    chairs[i] = depart;
-
-                    if(arrival == targetArrival){
-                        return i;
-                    }
-                break;
-                }
+        int targetArrival = times[targetFriend][0]; //[a,d]
+        Arrays.sort(times, new Comparator<int[]>(){
+            public int compare(int a[], int b[]){
+                return a[0] - b[0];
             }
+        });
+        PriorityQueue<int[]> occupied = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int a[], int b[]){
+                return a[0] - b[0];
+            }
+        });
+        PriorityQueue<Integer> available = new PriorityQueue<>();
+        int chairNo=0;
+        for(int time[] : times){
+            int arrTime = time[0];
+            int leavingTime = time[1];
+            while(!occupied.isEmpty() && occupied.peek()[0] <= arrTime){
+                available.offer(occupied.poll()[1]);
+            }
+            int currentChairNo;
+            if(available.isEmpty()){
+                currentChairNo = chairNo;
+                chairNo++;
+            }else{
+                currentChairNo = available.poll();
+            }
+            if(targetArrival == arrTime) return currentChairNo;
+            occupied.offer(new int[]{leavingTime, currentChairNo});
         }
         return -1;
     }
