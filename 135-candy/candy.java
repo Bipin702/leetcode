b1628b577986805c2f6764da9074ba31f961d4a7
Bipin <bipinchoubey771@gmail.com>
2026-01-26 02:48:51 +0530

    Time: 3 ms (88.71%) | Memory: 47.6 MB (63.66%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] result = new int[n];
+
+       for(int i = 0; i < n; i++) result[i] = 1;
+
+       for(int i = 1; i < n; i++){
+          if(ratings[i] > ratings[i-1]){
+            result[i] = result[i-1] + 1;
+          }
+       }
+
+       for(int i = n-1; i > 0; i--){
+            if(ratings[i-1] > ratings[i]){
+                result[i-1] = Math.max(result[i] + 1, result[i-1]);
+            }
+       }
+        int sum = 0;
+
+        for(int num : result) sum += num;
+
+        return sum;
+    }
+}
