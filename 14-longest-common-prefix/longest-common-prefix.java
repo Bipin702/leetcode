2cf484e345b547e0be26ac8e571a9a7be0969ff9
Bipin <bipinchoubey771@gmail.com>
2026-01-26 18:12:07 +0530

    Time: 1 ms (65.33%) | Memory: 43.4 MB (25.45%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+
+        String res = strs[0];
+
+        for(int i = 1; i < n; i++){
+            res = Common(res,strs[i]);
+        }
+        return res;
+    }
+
+    public String Common(String s1, String s2){
+        int n = Math.min(s1.length(),s2.length());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++){
+            if(s1.charAt(i) == s2.charAt(i)){
+                sb.append(s1.charAt(i));
+            }else{
+                break;
+            }
+        }
+        return sb.toString();
+    }
+}
