806e1223aea81443257e051949a1f79dedead208
Bipin <bipinchoubey771@gmail.com>
2026-01-26 18:55:30 +0530

    Time: 6 ms (83.02%) | Memory: 44.8 MB (30.17%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+        int n = arr.length;
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = n-1; i >= 0; i--){
+            sb.append(arr[i]);
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
