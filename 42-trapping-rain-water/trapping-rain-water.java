959d67fbbb3a15f8594aa5a38cfd097c0b0dfad0
Bipin <bipinchoubey771@gmail.com>
2026-01-26 15:34:00 +0530

    Time: 1 ms (60.31%) | Memory: 48.3 MB (37.64%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -2,34 +2,51 @@ class Solution {
     public int trap(int[] height) {
         int n = height.length;
 
-        int[] leftMax = LeftMax(height,n);
-        int[] rightMax = RightMax(height,n);
-
-        int sum = 0;
-
-        for(int i = 0; i < n; i++){
-            int h = Math.min(leftMax[i],rightMax[i]) - height[i];
-            sum += h;
-        }
-        return sum;
-    }
-
-    public int[] LeftMax(int[] height, int n){
         int[] leftMax = new int[n];
-        leftMax[0] = height[0];
-        for(int i = 1; i < n; i++){
-            leftMax[i] = Math.max(leftMax[i-1],height[i]);
+        int[] rightMax = new int[n];
+
+        leftMax = LeftMax(height);
+        rightMax = RightMax(height);
+
+        int water = 0;
+
+        for(int i = 1; i < n-1; i++){
+            water += Math.min(leftMax[i],rightMax[i]) - height[i];
         }
-        return leftMax;
+        return water;
     }
 
-    public int[] RightMax(int[] height, int n){
-        int[] rightMax = new int[n];
-        rightMax[n-1] = height[n-1];
+    public int[] LeftMax(int[] height){
+        int n = height.length;
+
+        int[] result = new int[n];
+        result[0] = height[0];
+        int leftMax = height[0];
+        for(int i = 1; i < n; i++){
+
+            if(height[i] > leftMax){
+                leftMax = height[i];
+            }
+
+            result[i] = leftMax;
+        }
+        return result;
+    }
+
+    public int[] RightMax(int[] height){
+        int n = height.length;
+
+        int[] result = new int[n];
+        result[n-1] = height[n-1];
+        int rightMax = height[n-1];
 
         for(int i = n-2; i >= 0; i--){
-            rightMax[i] = Math.max(rightMax[i+1],height[i]);
+            if(height[i] > rightMax){
+                rightMax = height[i];
+            }
+
+            result[i] = rightMax;
         }
-        return rightMax;
+        return result;
     }
 }
