1aa7e8209c2e132376a022b6a8cc734a1443aa79
Bipin <bipinchoubey771@gmail.com>
2026-01-31 15:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (65.72%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+
+        for(char ch : letters){
+            if(ch > target){
+                return ch;
+            }
+        }
+        return letters[0];
+    }
+}
