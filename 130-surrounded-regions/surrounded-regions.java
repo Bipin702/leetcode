0fbd67028687fbb2d73f711ea2eef07be00229c7
Bipin <bipinchoubey771@gmail.com>
2026-02-03 22:23:19 +0530

    Time: 2 ms (82.64%) | Memory: 47.9 MB (33.34%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,30 +2,27 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-
         int[][] visited = new int[n][m];
 
-        for(int j = 0; j < m; j++){
+        for(int i = 0; i < m; i++){
             //first row
-            if(visited[0][j] == 0 && board[0][j] == 'O'){
-                dfs(0,j,visited,board);
+            if(visited[0][i] == 0 && board[0][i] == 'O'){
+                dfs(0,i,visited,board);
             }
 
             //last row
-            if(visited[n-1][j] == 0 && board[n-1][j] == 'O'){
-                dfs(n-1,j,visited,board);
+            if(visited[n-1][i] == 0 && board[n-1][i] == 'O'){
+                dfs(n-1,i,visited,board);
             }
         }
 
         for(int i = 0; i < n; i++){
-            //first col
             if(visited[i][0] == 0 && board[i][0] == 'O'){
                 dfs(i,0,visited,board);
             }
 
-            //last  col
             if(visited[i][m-1] == 0 && board[i][m-1] == 'O'){
-                dfs(i,m-1,visited,board);
+                dfs(i,m-1, visited,board);
             }
         }
 
@@ -40,8 +37,10 @@ class Solution {
 
     public void dfs(int row, int col, int[][] visited, char[][] board){
         visited[row][col] = 1;
+
         int n = board.length;
         int m = board[0].length;
+
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
 
@@ -50,7 +49,7 @@ class Solution {
             int nCol = col + delCol[i];
 
             if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && visited[nRow][nCol] == 0 && board[nRow][nCol] == 'O'){
-                dfs(nRow,nCol,visited,board);
+                dfs(nRow, nCol,visited,board);
             }
         }
     }
