9f47209137052df6e14716c7b1190cd2af30cfec
Bipin <bipinchoubey771@gmail.com>
2026-02-05 19:04:20 +0530

    Time: 1 ms (98.72%) | Memory: 47 MB (13.78%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -2,22 +2,11 @@ class Solution {
     public int[] constructTransformedArray(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        for(int i = 0; i < n; i++){
-            int numIndex = i;
-            if(nums[i] > 0){
-                int index = nums[i]%n;
-                while(index > 0){
-                    numIndex = (numIndex+1)%n;
-                    index--;
-                }
-                res[i] = nums[numIndex];
-            }else if(nums[i] < 0){
-                int index = Math.abs(nums[i]%n);
-                numIndex = ((numIndex - index) % n + n) % n;
-                res[i] = nums[numIndex];
-            }else{
-                res[i] = nums[i];
-            }
+
+        for (int i = 0; i < n; i++) {
+            int shift = nums[i] % n;
+            int index = ((i + shift) % n + n) % n;
+            res[i] = nums[index];
         }
         return res;
     }
