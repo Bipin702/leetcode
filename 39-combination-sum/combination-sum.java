87cf8e3853caa487da33344f2c5849fa3b0c267a
Bipin <bipinchoubey771@gmail.com>
2026-02-06 16:26:16 +0530

    Time: 2 ms (95.15%) | Memory: 45.8 MB (66.24%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
-        return res;        
-    }
-
-    private void makeCombination(int[] candidates, int target, int idx, List<Integer> comb, int total, List<List<Integer>> res) {
-        if (total == target) {
-            res.add(new ArrayList<>(comb));
-            return;
-        }
-
-        if (total > target || idx >= candidates.length) {
-            return;
-        }
-
-        comb.add(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
-        comb.remove(comb.size() - 1);
-        makeCombination(candidates, target, idx + 1, comb, total, res);
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(0,candidates,target, new ArrayList<>(),ans);
+        return ans;
     }    
+
+    public void solve(int index, int[] candidates, int target,List<Integer> subPart, List<List<Integer>> ans){
+
+        int n = candidates.length;
+
+        if(target == 0){
+            ans.add(new ArrayList<>(subPart));
+            return;
+        }
+
+        if(index == n || target < 0) return;
+
+        subPart.add(candidates[index]);
+        solve(index,candidates,target-candidates[index],subPart,ans);
+        subPart.remove(subPart.size()-1);
+        solve(index+1,candidates,target,subPart,ans);
+    }
 }
