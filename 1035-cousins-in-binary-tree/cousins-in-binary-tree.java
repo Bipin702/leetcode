f4208f8b74acd96ced8d3efec8b54799b6e7a95c
Bipin <bipinchoubey771@gmail.com>
2026-02-08 19:44:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (18.74%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -18,7 +18,9 @@ class Solution {
         TreeNode xx = findNode(root,x);
         TreeNode yy = findNode(root,y);
 
-        return (level(root,xx,0) == level(root,yy,0)) && (!isSiblings(root,xx,yy));
+        return (
+            level(root,xx,0) == level(root,yy,0)
+        ) && (!isSibling(root,xx,yy));
     }
 
     public TreeNode findNode(TreeNode node, int x){
@@ -26,16 +28,10 @@ class Solution {
 
         if(node.val == x) return node;
 
-        TreeNode left = findNode(node.left,x);
-        if(left != null) return left;
-        
+        TreeNode l = findNode(node.left,x);
+        if(l != null) return l;
+
         return findNode(node.right,x);
-    }
-
-    public boolean isSiblings(TreeNode node, TreeNode x, TreeNode y){
-        if(node == null) return false;
-
-        return (node.left == x && node.right == y) || (node.left == y && node.right == x) ||isSiblings(node.left,x,y) || isSiblings(node.right,x,y);
     }
 
     public int level(TreeNode node, TreeNode x, int level){
@@ -43,9 +39,15 @@ class Solution {
 
         if(node == x) return level;
 
-        int left = level(node.left,x,level+1);
-        if(left != 0) return left;
+        int l = level(node.left,x,level+1);
+        if(l != 0) return l;
 
         return level(node.right,x,level+1);
+    }
+
+    public boolean isSibling(TreeNode node, TreeNode x, TreeNode y){
+        if(node == null) return false;
+
+        return (node.left == x && node.right == y) || (node.right == x && node.left == y) || isSibling(node.left,x,y) || isSibling(node.right,x,y);
     }
 }
