3d7ff14ce6021e39151fa5bccae7e38c750fa02c
Bipin <bipinchoubey771@gmail.com>
2026-02-08 20:25:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (48.74%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -17,13 +17,13 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return false;
 
-        return isMirror(root.left,root.right);
+        return isMirror(root.left, root.right);
     }
 
-    public boolean isMirror(TreeNode p , TreeNode q){
+    public boolean isMirror(TreeNode p, TreeNode q){
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
 
-        return p.val == q.val && isMirror(p.left,q.right) && isMirror(p.right,q.left);
+        return (p.val == q.val) && isMirror(p.left,q.right) && isMirror(p.right,q.left);
     }
 }
