31dee81d9b742346a2303351e378a6d8afcdfa1e
Bipin <bipinchoubey771@gmail.com>
2026-02-10 13:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 49.5 MB (44.04%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -17,30 +17,30 @@ class Solution {
     public int countNodes(TreeNode root) {
         if(root == null) return 0;
 
-        int leftHeight = getLeftHeight(root);
-        int rightHeight = getRightHeight(root);
+        int leftHeight = getLeft(root);
+        int rightHeight = getRight(root);
 
-        if(leftHeight == rightHeight){
-            return (1 << leftHeight) - 1;
-        }
+        if(leftHeight == rightHeight) return (int)Math.pow(2,leftHeight) - 1;
 
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
-    public int getLeftHeight(TreeNode node){
+    public int getLeft(TreeNode node){
+        if(node == null) return 0;
         int height = 0;
         while(node != null){
-            height++;
             node = node.left;
+            height++;
         }
         return height;
     }
 
-    public int getRightHeight(TreeNode node){
+    public int getRight(TreeNode node){
         int height = 0;
+
         while(node != null){
-            height++;
             node = node.right;
+            height++;
         }
         return height;
     }
