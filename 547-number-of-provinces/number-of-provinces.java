715159e7aef2b40bf477a24b80d27ea86b6f6f08
Bipin <bipinchoubey771@gmail.com>
2026-02-15 01:41:32 +0530

    Time: 5 ms (11.73%) | Memory: 49.5 MB (16.12%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,38 +1,36 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int result = 0;
+        int m = isConnected[0].length;
+
         List<List<Integer>> adj = new ArrayList<>();
-
         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
-
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < m; j++){
                 if(isConnected[i][j] == 1 && i != j){
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
-
+        int count = 0;
         int[] visited = new int[n];
 
         for(int i = 0; i < n; i++){
             if(visited[i] == 0){
-                result++;
-                dfs(i,adj,visited);
+                count++;
+                dfs(i,visited,adj);
             }
         }
-        return result;
+        return count;
     }
 
-    public void dfs(int node, List<List<Integer>> adj,int[] visited){
+    public void dfs(int node, int[] visited, List<List<Integer>> adj){
         visited[node] = 1;
 
         for(int neighbor : adj.get(node)){
-            if(visited[neighbor] == 0){
-                dfs(neighbor,adj,visited);
-            }
+            if(visited[neighbor] == 0)
+            dfs(neighbor,visited,adj);
         }
     }
 }
