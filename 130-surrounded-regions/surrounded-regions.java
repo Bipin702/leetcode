d63f9432b75222fb5ce8e7b9e0c7582c10ac5ea4
Bipin <bipinchoubey771@gmail.com>
2026-02-16 02:43:12 +0530

    Time: 2 ms (81.62%) | Memory: 47.8 MB (74.01%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -5,7 +5,7 @@ class Solution {
         int[][] visited = new int[n][m];
 
         for(int i = 0; i < m; i++){
-            //first row
+            // first row
             if(visited[0][i] == 0 && board[0][i] == 'O'){
                 dfs(0,i,visited,board);
             }
@@ -22,24 +22,23 @@ class Solution {
             }
 
             if(visited[i][m-1] == 0 && board[i][m-1] == 'O'){
-                dfs(i,m-1, visited,board);
+                dfs(i,m-1,visited,board);
             }
         }
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(visited[i][j] == 0 && board[i][j] == 'O'){
-                    board[i][j] = 'X';
-                }
+                if(visited[i][j] == 0 && board[i][j] == 'O')
+                board[i][j] = 'X';
             }
         }
     }
 
     public void dfs(int row, int col, int[][] visited, char[][] board){
-        visited[row][col] = 1;
-
         int n = board.length;
         int m = board[0].length;
+
+        visited[row][col] = 1;
 
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
@@ -49,7 +48,7 @@ class Solution {
             int nCol = col + delCol[i];
 
             if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && visited[nRow][nCol] == 0 && board[nRow][nCol] == 'O'){
-                dfs(nRow, nCol,visited,board);
+                dfs(nRow,nCol,visited,board);
             }
         }
     }
