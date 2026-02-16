88ad16ed3035518a4f3213472e6b90be19593927
Bipin <bipinchoubey771@gmail.com>
2026-02-16 15:33:52 +0530

    Time: 16 ms (58.56%) | Memory: 61.8 MB (59.44%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,46 +1,46 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        int m = graph[0].length;
        List<List<Integer>> adj = new ArrayList<>();
-       int n = graph.length;
 
        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
 
        for(int i = 0; i < n; i++){
-        for(int v : graph[i]){
-            adj.get(i).add(v);
-        }
-    }
-
-
-       int[] visited = new int[n];
-       int[] Check = new int[n];
-       int[] pathVisited = new int[n];
-
-       for(int i = 0; i < n; i++){
-            if(visited[i] == 0){
-                dfs(i,visited,Check,pathVisited,adj);
+            for(int neighbor : graph[i]){
+                adj.get(i).add(neighbor);
             }
         }
 
-        List<Integer> safeNodes = new ArrayList<>();
 
-        for(int i = 0; i < n; i++){
-            if(Check[i] == 1) safeNodes.add(i);
+       int[] visited = new int[n];
+       int[] pathVisited = new int[n];
+       int[] check = new int[n];
+       List<Integer> ans = new ArrayList<>();
+
+       for(int i = 0; i < n; i++){
+        if(visited[i] == 0){
+            dfs(i,visited,pathVisited,check,adj);
         }
-        return safeNodes;
+       }
+       
+       for(int i = 0; i < n; i++){
+        if(check[i] == 1) ans.add(i);
+       }
+
+       return ans;
     }
 
-    public boolean dfs(int node, int[] visited, int[] check, int[] pathVisited, List<List<Integer>> adj){
+    public boolean dfs(int node, int[] visited, int[] pathVisited, int[] check, List<List<Integer>> adj){
         visited[node] = 1;
         check[node] = 0;
         pathVisited[node] = 1;
 
         for(int neighbor : adj.get(node)){
             if(visited[neighbor] == 0){
-                if(dfs(neighbor,visited,check,pathVisited,adj)) return true;
+                if(dfs(neighbor,visited,pathVisited,check,adj)) return true;
             }else if(pathVisited[neighbor] == 1) return true;
         }
-
         pathVisited[node] = 0;
         check[node] = 1;
         return false;
